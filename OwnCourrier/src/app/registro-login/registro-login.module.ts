090b5074eaf18703d1a9816ee3afbdb94ac3783f
ricardobeta <45794359+ricardobeta@users.noreply.github.com>
2021-01-07 15:27:15 -0500

    Modulos Creados

---
+++ OwnCourrier/src/app/registro-login/registro-login.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RegistroLoginRoutingModule } from './registro-login-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RegistroLoginRoutingModule
+  ]
+})
+export class RegistroLoginModule { }
