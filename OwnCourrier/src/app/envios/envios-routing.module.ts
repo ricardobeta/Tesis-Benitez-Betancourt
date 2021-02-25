d4faf3257382a853968dfc119d516e7cff4a28b8
ricardobeta <45794359+ricardobeta@users.noreply.github.com>
2021-02-24 20:32:54 -0500

    Update envios-routing.module.ts

--- OwnCourrier/src/app/envios/envios-routing.module.ts
+++ OwnCourrier/src/app/envios/envios-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrincipalComponent } from './components/principal/principal.component';
+import { RegistroEnvioComponent } from './components/registro-envio/registro-envio.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: PrincipalComponent
+  },
+  {
+    path: 'registro',
+    component: RegistroEnvioComponent
   }
 ];
 
