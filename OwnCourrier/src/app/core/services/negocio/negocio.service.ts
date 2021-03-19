1ab7f2807f2c55393c2d37d3802d6e029c618067
ricardobeta <45794359+ricardobeta@users.noreply.github.com>
2021-03-18 22:37:27 -0500

    Update negocio.service.ts

--- OwnCourrier/src/app/core/services/negocio/negocio.service.ts
+++ OwnCourrier/src/app/core/services/negocio/negocio.service.ts
@@ -9,17 +9,14 @@ import { Negocio } from '../../models/negocio';
 })
 export class NegocioService {
 
-  public idNegocio = new BehaviorSubject<string>(null);
+  public idNegocio = new BehaviorSubject<string>(undefined);
   public negocio = new BehaviorSubject<Negocio>(undefined);
 
   constructor(private db: AngularFireDatabase,
               private storage: AngularFireStorage) { }
 
-  recuperarNegocio(id) {
-    const sub$ = this.db.object(`Negocios/${id}`).snapshotChanges().subscribe( auxNegocio => {
-      this.idNegocio.next(auxNegocio.key);
-      this.negocio.next(auxNegocio.payload.toJSON() as Negocio);
-      sub$.unsubscribe();
-    });
-  }  
+
+  informacionNegocio() {
+    return this.db.object(`Negocios/${this.idNegocio.value}`).valueChanges()
+  }
 }
