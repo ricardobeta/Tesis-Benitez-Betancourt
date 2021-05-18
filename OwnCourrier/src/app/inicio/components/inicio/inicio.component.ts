1460d040bdcd25a8a6b80ad71b7a7c9e66b6ac2c
ChristianB <christian.benitez@epn.edu.ec>
2021-05-17 23:07:43 -0500

    Problemas en envíos

--- OwnCourrier/src/app/inicio/components/inicio/inicio.component.ts
+++ OwnCourrier/src/app/inicio/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 import { Negocio } from 'src/app/core/models/negocio';
 import { LoginService } from 'src/app/core/services/login/login.service';
 import { NegocioService } from 'src/app/core/services/negocio/negocio.service';
@@ -10,8 +11,25 @@ import { NegocioService } from 'src/app/core/services/negocio/negocio.service';
 })
 export class InicioComponent implements OnInit {
 
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+
   banderaCargando = true;
   negocio: Negocio;
+
+  folders = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
 
   constructor(private negociosService: NegocioService,
               private loginService: LoginService) { }
